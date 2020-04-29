064ccf04fa75e424fbccc18251ad367af4198cb8
s19023 <s19023@pja.edu.pl>
2020-04-29 19:26:25 +0200

    Add: reading neighbouring powiats from neighbours.json

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import geopandas, random, contextily
+import json, geopandas, pandas, random, contextily
 import matplotlib.pyplot as plt
 
 powiaty = geopandas.read_file('map-data/powiaty.shp', encoding = 'utf-8')
-print(powiaty.head())
+with open('map-data/neighbours.json', 'r') as neighbours_file:
+    neighbours = json.load(neighbours_file)
 
 cmap = plt.get_cmap('tab20')
 fig, ax = plt.subplots(figsize = (8,8))
@@ -11,7 +12,7 @@ ax.set_aspect('equal')
 
 for i in range(len(powiaty)):
     row = powiaty.loc[[i],]
-    row.plot(ax = ax, color = cmap(row['density']), edgecolor = 'k', linewidth = 0.3)
+    row.plot(ax = ax, color = cmap(row['value']), edgecolor = 'k', linewidth = 0.3)
     
 contextily.add_basemap(ax, source = contextily.sources.ST_TERRAIN_BACKGROUND, zoom = 9)
 plt.show(ax)
