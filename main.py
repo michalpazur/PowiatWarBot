b8ccfbf91d7a31321384532470390019bd30f0b5
s19023 <s19023@pja.edu.pl>
2020-04-30 00:32:50 +0200

    Update: merging geometry for better plotting

--- main.py
+++ main.py
@@ -1,50 +1,62 @@
 import json, geopandas, pandas, random, contextily, re
 import matplotlib.pyplot as plt
 
+pandas.set_option('mode.chained_assignment', None)
+
 powiaty = geopandas.read_file('map-data/powiaty.shp', encoding = 'utf-8')
 with open('map-data/neighbours.json', 'r') as neighbours_file:
     neighbours = json.load(neighbours_file)
 
-for i in range(10):
-    can_poceed = False
+for i in range(500):
+    can_proceed = False
 
-    while not can_poceed:
+    while not can_proceed:
         if i == 0 or random.random() < 0.6:
             random_powiat_row = powiaty.loc[[random.choice(powiaty.index)]]
         else:
             random_powiat_row = all_rows_for_conquering_powiat.loc[[random.choice(all_rows_for_conquering_powiat.index)]]
 
+        #get a random powiat and its owner
         random_powiat_code = random_powiat_row['code'].iloc[0]
         random_powiat_belongs_to = random_powiat_row['belongs_to'].iloc[0]
         conquering_powiat_row = powiaty[powiaty['code'] == random_powiat_belongs_to]
         conquering_powiat_code = conquering_powiat_row['code'].iloc[0]
         conquering_powiat_value = conquering_powiat_row['value'].iloc[0]
 
+        #get a powiat to conquer from the list of neighbors
         powiat_to_conquer_code = random.choice(neighbours[random_powiat_code])
         powiat_to_conquer_row = powiaty[powiaty['code'] == powiat_to_conquer_code]
         powiat_to_conquer_belongs_to_code = powiat_to_conquer_row['belongs_to'].iloc[0]
     
-        can_poceed = (powiat_to_conquer_belongs_to_code != conquering_powiat_code)
+        #powiat can only be conquered if it doesn't belong to conquering powiat
+        can_proceed = (powiat_to_conquer_belongs_to_code != conquering_powiat_code)
     
     conquering_powiat_name = conquering_powiat_row['name'].iloc[0]
     powiat_to_conquer_name = powiat_to_conquer_row['name'].iloc[0]
+
+    #merge geometry for conquering powiat
     all_rows_for_conquering_powiat = powiaty[powiaty['belongs_to'] == conquering_powiat_code]
+    conquering_powiat_geometry = all_rows_for_conquering_powiat['geometry'].unary_union
+    conquering_powiat_row['geometry'].iloc[0] = conquering_powiat_geometry
 
     #find row for conquered powiat owner
-    powiat_to_conquer_belongs_to_row = powiaty[powiaty['code'] == powiat_to_conquer_belongs_to_code]
-    powiat_to_conquer_belongs_to_name = powiat_to_conquer_belongs_to_row['name'].iloc[0]
-    powiat_to_conquer_belongs_to_value = powiat_to_conquer_belongs_to_row['value'].iloc[0]
+    powiat_to_conquer_owner_row = powiaty[powiaty['code'] == powiat_to_conquer_belongs_to_code]
+    powiat_to_conquer_owner_name = powiat_to_conquer_owner_row['name'].iloc[0]
+    powiat_to_conquer_owner_value = powiat_to_conquer_owner_row['value'].iloc[0]
 
     #update values for conquered powiat
     powiaty['belongs_to'][powiaty['code'] == powiat_to_conquer_code] = conquering_powiat_code
     powiaty['value'][powiaty['code'] == powiat_to_conquer_code] = conquering_powiat_value
 
-    print('{} podbija {} należący do {}'.format(conquering_powiat_name, powiat_to_conquer_name, powiat_to_conquer_belongs_to_name))
+    print('{} podbija {} należący do {}'.format(conquering_powiat_name, powiat_to_conquer_name, powiat_to_conquer_owner_name))
 
-    #find all rows for conquered powiat owner
+    #find all rows for conquered powiat owner and merge geometry
+    all_rows_for_powiat_to_conquer_owner = powiaty[powiaty['belongs_to'] == powiat_to_conquer_belongs_to_code]
+    powiat_to_conquer_owner_geometry = all_rows_for_powiat_to_conquer_owner['geometry'].unary_union
+    powiat_to_conquer_owner_row['geometry'].iloc[0] = powiat_to_conquer_owner_geometry
 
-    if (powiaty[powiaty['belongs_to'] == powiat_to_conquer_belongs_to_code].empty):
-        print('🦀 {} is gone 🦀'.format(powiat_to_conquer_belongs_to_name))
+    if (all_rows_for_powiat_to_conquer_owner.empty):
+        print('🦀 {} is gone 🦀'.format(powiat_to_conquer_owner_name))
 
 cmap = plt.get_cmap('tab20')
 fig, ax = plt.subplots(figsize = (8,8))
@@ -56,6 +68,8 @@ for i in range(len(powiaty)):
     row.plot(ax = ax, color = cmap(row['value']), edgecolor = 'k', linewidth = 0.3)
     
 conquering_powiat_row.plot(ax = ax, color = cmap(conquering_powiat_value), edgecolor = 'green', linewidth = 2)
-powiat_to_conquer_belongs_to_row.plot(ax = ax, color = cmap(conquering_powiat_value), edgecolor = 'red', hatch = '///', linewidth = 2)
+powiat_to_conquer_row.plot(ax = ax, color = cmap(powiat_to_conquer_owner_value), edgecolor = 'red', hatch = '///', linewidth = 2)
+powiat_to_conquer_owner_row.plot(ax = ax, color = cmap(powiat_to_conquer_owner_value), edgecolor = 'blue', linewidth = 2)
+
 contextily.add_basemap(ax, source = contextily.sources.ST_TERRAIN_BACKGROUND, zoom = 9)
 plt.show(ax)
